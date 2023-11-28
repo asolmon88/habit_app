a9d61d30c576ce05893bfa59b9e34826d1f7c436
Ariel <ariel@debian-BULLSEYE-live-builder-AMD64>
2023-11-27 20:39:57 -0600

    Adds structure

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:habit_app/presentation/bloc.dart';
+import 'package:habit_app/presentation/screens.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  // redirect: (context, state) {
+  //   final isAuth = context.read<AuthCubit>().state.isAuth;
+  //   final isCreatingAccount = context.read<AuthCubit>().state.isCreatingAccount;
+  //   if (!isAuth && !isCreatingAccount) {
+  //     return '/login';
+  //   } else {
+  //     return null;
+  //   }
+  // },
+  routes: [
+    GoRoute(
+      path: '/',
+      builder:(context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/login',
+      builder:(context, state) => LoginScreen(),
+    ),
+  ]
+);
