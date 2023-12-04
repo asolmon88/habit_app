597a5f7458f4608040c049f353b00c1063f67c59
Ariel <ariel@debian-BULLSEYE-live-builder-AMD64>
2023-12-04 15:25:41 -0600

    You can now modify habits progress

--- lib/presentation/bloc/cubit/habits_state.dart
+++ lib/presentation/bloc/cubit/habits_state.dart
@@ -3,6 +3,8 @@ part of 'habits_cubit.dart';
 class HabitsState extends Equatable {
 
   final bool isLoading;
+  final bool error;
+  final String errorMessage;
 
   final List<Map<String, dynamic>> dailyHabits;
   final List<Map<String, dynamic>> monthlyHabits;
@@ -10,6 +12,8 @@ class HabitsState extends Equatable {
 
   const HabitsState({
     this.isLoading = false,
+    this.error = false,
+    this.errorMessage = '',
     this.dailyHabits = const [],
     this.monthlyHabits = const [],
     this.yearlyHabits = const [],
@@ -17,17 +21,22 @@ class HabitsState extends Equatable {
 
   HabitsState copywith({
     bool? isLoading,
+    bool? error,
+    String? errorMessage,
     List<Map<String, dynamic>>? dailyHabits,
     List<Map<String, dynamic>>? monthlyHabits,
     List<Map<String, dynamic>>? yearlyHabits,
 
   }) => HabitsState(
     isLoading: isLoading ?? this.isLoading,
+    error: error ?? this.error,
+    errorMessage: errorMessage ?? this.errorMessage,
     dailyHabits: dailyHabits ?? this.dailyHabits,
     monthlyHabits: monthlyHabits ?? this.monthlyHabits,
     yearlyHabits: yearlyHabits ?? this.yearlyHabits,
   );
 
   @override
-  List<Object> get props => [isLoading, dailyHabits, monthlyHabits];
+  List<Object> get props => [isLoading, dailyHabits, monthlyHabits,
+    error, errorMessage];
 }
---
+++ lib/presentation/widgets/habits_list/habits_list.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:habit_app/presentation/bloc.dart';
+import 'package:habit_app/presentation/widgets.dart';
+
+class HabitsList extends StatefulWidget {
+
+  final int rowCount;
+  final int columnCount;
+  final Map<String, dynamic>? habits;
+  final bool edit;
+
+  const HabitsList({
+    super.key,
+    this.rowCount = 1,
+    this.columnCount = 2,
+    this.habits,
+    this.edit = false,
+  });
+
+  @override
+  State<HabitsList> createState() => _HabitsListState();
+}
+
+class _HabitsListState extends State<HabitsList> {
+  void editProgress(
+    String habitName, HabitsCubit habitsCubit, String email,
+    String endValue, String currentValue
+  ) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const TitleText(title: 'Edit Exercise'),
+          content: EditForm(
+            currentValue: currentValue,
+            endValue: endValue,
+            onPressedSave: (value) {
+              habitsCubit.updateHabit(email, 'daily_habits',
+                  habitName, value);
+              context.pop();
+            },
+            onPressedComplete: () {
+              habitsCubit.updateHabit(email, 'daily_habits',
+                habitName, endValue);
+              context.pop();
+            }
+          )
+        );
+      }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final habitsCubit = context.watch<HabitsCubit>();
+
+    return Column(
+      children: List.generate(
+        widget.rowCount,
+        (rowIndex) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(
+              widget.columnCount,
+              (columnIndex) {
+                final index = rowIndex * widget.columnCount + columnIndex;
+                if (widget.habits != null) {
+                  if (index != widget.habits!.length) {
+                    final title = widget.habits!.keys.elementAt(index);
+                    final currentHabit = widget.habits![title];
+                    return Flexible(
+                      child: HabitCard(
+                        title: title,
+                        value: int.parse(currentHabit['current_value'])
+                              /int.parse(currentHabit['end_value']),
+                        endValue: currentHabit['end_value'],
+                        currentValue: currentHabit['current_value'],
+                        metric: currentHabit['metric'],
+                        edit: widget.edit,
+                        onTap: () => editProgress(title, habitsCubit,
+                          context.read<AuthCubit>().state.email,
+                          currentHabit['end_value'],
+                          currentHabit['current_value']),
+                      ),
+                    );
+                  } else {
+                    return const Expanded(child: SizedBox());
+                  }
+                } else {
+                  return const Text('All done for today!');
+                }
+              }
+            ),
+          );
+        }
+      ),
+    );
+  }
+}
